7e7bbc2e93fe5d434b4d9aac5ccb7c73d8a8dd9e
bartlomiejfydrych <fydbar@gmail.com>
2020-02-23 16:11:43 +0100

    Add abTesting

--- src/test/java/theinternet/Internet.java
+++ src/test/java/theinternet/Internet.java
@@ -1,6 +1,10 @@
 package theinternet;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -13,10 +17,21 @@ public class Internet {
 
     @Before
     public void setUp(){
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
         driver.manage().window().maximize();
         driver.get("http://the-internet.herokuapp.com/");
     }
 
+    @Test
+    public void abTesting(){
+        driver.findElement(By.linkText("A/B Testing")).click();
+
+        String isTextDisplayed = driver.findElement(By.cssSelector("div.example > p")).getText();
+        Assert.assertEquals("Also known as split testing. This is a way in which businesses are able" +
+                " to simultaneously test and learn different versions of a page to see which text and/or" +
+                " functionality works best towards a desired outcome (e.g. a user action such as" +
+                " a click-through).", isTextDisplayed);
+    }
 }
