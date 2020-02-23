df76b7c691a49e2cc7a6ff676a91ed833215ea0a
bartlomiejfydrych <fydbar@gmail.com>
2020-02-23 14:58:14 +0100

    Add @Before annotation

--- src/test/java/theinternet/Internet.java
+++ src/test/java/theinternet/Internet.java
@@ -1,7 +1,22 @@
 package theinternet;
 
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 
 public class Internet {
 
+    WebDriver driver;
+
+    @Before
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://the-internet.herokuapp.com/");
+    }
 
 }
